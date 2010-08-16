ffe881ca6ee5b16f137087e8f9f019d14adce828
Mark Ryall <mark@ryall.name>
2010-08-16 17:26:15 +1000

    added some methods to repository to get access to entry history and entries at particular changesets

--- lib/flat_hash/repository.rb
+++ lib/flat_hash/repository.rb
@@ -3,16 +3,24 @@ require 'flat_hash/directory'
 require 'flat_hash/git'
 require 'flat_hash/hg'
 
-module FlatHash
-  class Repository < Directory
-    extend Forwardable
-    def_delegators :@vcs, :changesets
+class FlatHash::Repository < FlatHash::Directory
+  extend Forwardable
+  def_delegators :@vcs, :changesets
 
-    def initialize serialiser, path
-      super
-      @vcs = Git.new if File.exist?('.git')
-      @vcs = Hg.new if File.exist?('.hg')
-      raise "could not determine repository type" unless @vcs
-    end
+  def initialize serialiser, path
+    super
+    @vcs = FlatHash::Git.new if File.exist?('.git')
+    @vcs = FlatHash::Hg.new if File.exist?('.hg')
+    raise "could not determine repository type" unless @vcs
+  end
+
+  def history key=nil
+    path = key ? File.join(@path, key) : @path
+    @vcs.changesets(path).map {|cs| @vcs.changeset cs }
+  end
+
+  def element_at changeset, key
+    content = @vcs.content_at(File.join(@path, key),changeset)
+    @serialiser.read StringIO.new(content)
   end
 end
