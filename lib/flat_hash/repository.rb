e1db37611ff6a7cc7e542c332ea703f0fe665c6f
Mark Ryall <mark@ryall.name>
2010-08-18 17:53:25 +1000

    changed repository to disable vcs features rather than raising exception when no vcs can be detected

--- lib/flat_hash/repository.rb
+++ lib/flat_hash/repository.rb
@@ -11,15 +11,20 @@ class FlatHash::Repository < FlatHash::Directory
     super
     @vcs = FlatHash::Git.new if File.exist?('.git')
     @vcs = FlatHash::Hg.new if File.exist?('.hg')
-    raise "could not determine repository type" unless @vcs
+  end
+
+  def vcs_supported?
+    !@vcs.nil?
   end
 
   def history key=nil
+    return [] unless vcs_supported?
     path = key ? File.join(@path, key) : @path
     @vcs.changesets(path).map {|cs| @vcs.changeset cs }
   end
 
   def element_at changeset, key
+    return {} unless vcs_supported?
     content = @vcs.content_at(File.join(@path, key),changeset)
     @serialiser.read StringIO.new(content)
   end
